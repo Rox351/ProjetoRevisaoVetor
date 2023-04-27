33d8e0c27765076bb076127dedbc465f179ea4ad
Leonardo Soares <95651574+Rox351@users.noreply.github.com>
2023-04-27 20:31:16 -0300

    27/04/2023

--- src/app/Exemplo_01.java
+++ src/app/Exemplo_01.java
@@ -11,7 +11,17 @@ public class Exemplo_01 {
 		
 		for(int i = 0; i < teste1.length; i++) {
 			num = JOptionPane.showInputDialog("Digite o valor " + i + ":");
+			teste1[i] = Integer.parseInt(num);
+		}
+		
+		for(int i = 0; i < teste1.length; i++) {
+			if(i%2 == 0) 
+				teste1[i] = teste1[i] * 5;
+			else
+				teste1[i] = teste1[i] + 5;
+			}
+		System.out.printf("%s\n" , "Resultado: ");
 		}
 	}
 
-}
+
