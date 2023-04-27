590d9d70ff754a29c147a67d94ee4afcfe17b634
Leonardo Soares <95651574+Rox351@users.noreply.github.com>
2023-04-27 20:17:57 -0300

    27/04/2023

--- src/app/Exemplo_01.java
+++ src/app/Exemplo_01.java
@@ -1,8 +1,17 @@
 package app;
 
+import javax.swing.JOptionPane;
+
 public class Exemplo_01 {
 	public static void main(String[] args) {
 		int [] teste1 = new int[10];
+		int [] teste2 = new int[10];
+		
+		String num;
+		
+		for(int i = 0; i < teste1.length; i++) {
+			num = JOptionPane.showInputDialog("Digite o valor " + i + ":");
+		}
 	}
 
 }
